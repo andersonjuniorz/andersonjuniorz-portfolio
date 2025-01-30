57363e2957b5b82493059aabf14d9f1dd976bbbe
andersonjuniorz <dev.andersonjuniorz@gmail.com>
2025-01-30 04:54:28 -0300

    ajustando paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfoliowebComponent } from './pages/portfolioweb/portfolioweb.component';
+import { PortfoliogamesComponent } from './pages/portfoliogames/portfoliogames.component';
+import { Portfolio3dComponent } from './pages/portfolio3d/portfolio3d.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : "" , component : HomeComponent },
+    { path : "about" , component : AboutComponent },
+    { path : "services/web" , component : PortfoliowebComponent },
+    { path : "services/games" , component : PortfoliogamesComponent },
+    { path : "services/3d" , component : Portfolio3dComponent }
+];
