367f506d91af8df43b1a4cd8100bd81a2083b476
andersonjuniorz <dev.andersonjuniorz@gmail.com>
2025-03-09 23:59:49 -0300

    Improving and organizing coding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: 
-  `<app-navbar />
+  `
+  <app-navbar />
   <router-outlet />
+  <app-footer />
   `,
 })
 export class AppComponent {
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterModule,
+    MatMenuModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 
-export class FooterComponent {}
+export class FooterComponent {
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object, // Injeta o identificador da plataforma
+    @Inject(DOCUMENT) private document: Document // Usa DOCUMENT para manipulacao segura do DOM
+  ) {}
+
+  goToContact() {
+    if (isPlatformBrowser(this.platformId)) { // Garante que so rode no navegador
+      
+      if (this.router.url === '/') {
+
+        const contactSection = this.document.getElementById('contact');
+
+        if (contactSection) {
+          contactSection.scrollIntoView({ behavior: 'smooth' });
+        }
+
+      } else {
+        
+        this.router.navigate(['/']).then(() => {
+          
+          setTimeout(() => {
+            const contactSection = this.document.getElementById('contact');
+            if (contactSection) {
+              contactSection.scrollIntoView({ behavior: 'smooth' });
+            }
+          }, 300);
+
+        });
+
+      }
+    }
+  }
+
+}
