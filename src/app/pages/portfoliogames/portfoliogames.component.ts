c160c65c3782bf5464e94ec6bfc1a144fd04ffd6
andersonjuniorz <dev.andersonjuniorz@gmail.com>
2025-03-10 02:15:18 -0300

    improving the game portfolio page

--- src/app/pages/portfoliogames/portfoliogames.component.ts
+++ src/app/pages/portfoliogames/portfoliogames.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { CardButtonComponent } from '../../components/card-button/card-button.component';
 
 @Component({
   selector: 'app-portfoliogames',
-  imports: [],
+  imports: [MatCardModule, CardButtonComponent],
   templateUrl: './portfoliogames.component.html',
   styleUrl: './portfoliogames.component.scss'
 })
-export class PortfoliogamesComponent {
 
-}
+export class PortfoliogamesComponent {}
