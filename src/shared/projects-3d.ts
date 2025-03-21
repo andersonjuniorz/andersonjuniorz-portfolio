868047896217e74505d92bc4dcd8ed52abf8739c
andersonjuniorz <dev.andersonjuniorz@gmail.com>
2025-03-21 00:07:30 -0300

    commenting on projects that need updating

---
+++ src/shared/projects-3d.ts
@@ -0,0 +1,19 @@
+import { CardItem } from "../app/interfaces/cardItem.interface";
+
+export const projects3D: CardItem[] =
+[
+    // { 
+    //     title: 'Escudo', 
+    //     description: 'Descrição do projeto 1', 
+    //     image: 'header-bg.png', 
+    //     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?ur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?',
+    //     technologies: ['Blender']
+    // },
+    // { 
+    //     title: 'Dino', 
+    //     description: 'Descrição do projeto 2', 
+    //     image: 'header-bg.png', 
+    //     text: 'LoreLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio? , distinctio?m ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?',
+    //     technologies: ['Blender']
+    // }
+];
---
+++ src/shared/projects-games.ts
@@ -0,0 +1,40 @@
+import { CardItem } from "../app/interfaces/cardItem.interface";
+
+export const projectsGames: CardItem[] =
+[ 
+    // { 
+    //     title: 'Estagiário Na Usina', 
+    //     description: 'Descrição do projeto 1', 
+    //     image: 'header-bg.png',
+    //     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?ur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?', 
+    //     technologies: ['Unity', 'C_Sharp', 'Blender']
+    // },
+    // { 
+    //     title: 'Argonauta (PC & VR)', 
+    //     description: 'Descrição do projeto 1', 
+    //     image: 'header-bg.png', 
+    //     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?ur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?', 
+    //     technologies: ['Unity', 'C_Sharp', 'Blender']
+    // },
+    // { 
+    //     title: 'Jogo do Mosquito (Remake)', 
+    //     description: 'Descrição do projeto 2', 
+    //     image: 'header-bg.png', 
+    //     text: 'LoreLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio? , distinctio?m ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?',
+    //     technologies: ['Unity', 'C_Sharp', 'Blender']
+    // },
+    // { 
+    //     title: 'Simulador de Treinamento de Emergência', 
+    //     description: 'Descrição do projeto 2', 
+    //     image: 'header-bg.png', 
+    //     text: 'LoreLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio? , distinctio?m ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?',
+    //     technologies: ['Unity', 'C_Sharp', 'Blender']
+    // },
+    // { 
+    //     title: 'Jogo da Reserva', 
+    //     description: 'Descrição do projeto 2', 
+    //     image: 'header-bg.png', 
+    //     text: 'LoreLorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio? , distinctio?m ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur minus magni ab possimus, velit rerum ullam numquam corporis ipsam dolores, tenetur eaque obcaecati iusto tempora. Iste, esse voluptate. Unde, distinctio?',
+    //     technologies: ['Unity', 'C_Sharp', 'Blender']
+    // }
+];
